Mesa-Labs-Archive/android_kernel_samsung_universal7885

selinux: togglable selinux status
This commit allows to enable/disable via config @jesec Fake Enforcing Status patch or @jcadduono SELinux Force Enforcing/Permissive patch

Signed-off-by: BlackMesa123 <brother12@hotmail.it>

security/selinux/ss/services.c
@@ -775,9 +775,13 @@ out:
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_SECURITY_SEC_SELINUX
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
+#endif
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+#if !defined(CONFIG_RKP_KDP)
+	selinux_enforcing = 0;
 #endif
 #endif
 #endif
@@ -1547,9 +1551,13 @@ out:
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_SECURITY_SEC_SELINUX
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
+#endif
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+#if !defined(CONFIG_RKP_KDP)
+	selinux_enforcing = 0;
 #endif
 #endif
 #endif
@@ -1847,8 +1855,10 @@ static inline int convert_context_handle_invalid_context(struct context *context
 
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_SECURITY_SEC_SELINUX
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+	selinux_enforcing = 0;
 #endif
 #endif
 // ] SEC_SELINUX_PORTING_COMMON

security/selinux/ss/status.c
@@ -60,8 +60,10 @@ struct page *selinux_kernel_status_page(void)
 			status->sequence = 0;
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_SECURITY_SEC_SELINUX
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 			status->enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+			status->enforcing = 0;
 #else
 			status->enforcing = selinux_enforcing;
 #endif

tnakamur/android_kernel_samsung_sc02l

security/selinux/ss/services.c
@@ -774,9 +774,13 @@ out:
 	kfree(t);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
+#endif
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+#if !defined(CONFIG_RKP_KDP)
+	selinux_enforcing = 0;
 #endif
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
@@ -1544,9 +1548,13 @@ out:
 	kfree(n);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 #if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
+#endif
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+#if !defined(CONFIG_RKP_KDP)
+	selinux_enforcing = 0;
 #endif
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
@@ -1842,8 +1850,10 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	u32 len;
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+	selinux_enforcing = 0;
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
 	if (selinux_enforcing)

security/selinux/ss/status.c
@@ -59,8 +59,10 @@ struct page *selinux_kernel_status_page(void)
 			status->version = SELINUX_KERNEL_STATUS_VERSION;
 			status->sequence = 0;
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 			status->enforcing = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+			status->enforcing = 0;
 #else
 			status->enforcing = selinux_enforcing;
 #endif